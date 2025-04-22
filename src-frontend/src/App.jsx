535b73ae87b198465ef3a3ebd938e8cb2550520f
Fabricio Ribeiro <fabricio.ribeiro@outlook.com>
2025-04-22 01:58:16 +0100

    Add Navbar Placeholder and logout functionality

--- src-frontend/src/App.jsx
+++ src-frontend/src/App.jsx
@@ -1,22 +1,22 @@
-
-import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Homepage from './Homepage'
-import Login from './user/Login'
-import Register from './user/Register'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import Homepage from "./Homepage";
+import Login from "./user/Login";
+import Register from "./user/Register";
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<Navigate to="/"/>} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
