e4a515b3ca286d167542a60a51ecf7ac1cbe8e24
Fabricio Ribeiro <fabricio.ribeiro@outlook.com>
2025-05-22 20:10:31 +0100

    Extend NavBar unit test

--- src-frontend/src/page/Navbar.test.jsx
+++ src-frontend/src/page/Navbar.test.jsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../store/store.jsx";
 import Navbar from "./Navbar";
+import { login } from "../store/authSlice";
 
 function renderWithProviders(ui) {
   return render(
@@ -18,6 +19,29 @@ describe("Navbar", () => {
     expect(screen.getByRole("button", { name: /login/i })).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: /sign up/i })
+    ).toBeInTheDocument();
+  });
+
+  it("shows user-specific options when logged in", async () => {
+    store.dispatch(
+      login.fulfilled({ token: "mockToken" }, "login", {
+        username: "testuser",
+        password: "password",
+      })
+    );
+
+    store.dispatch({
+      type: "auth/getUserData/fulfilled",
+      payload: { username: "testuser", first_name: "Test", last_name: "User" },
+    });
+
+    renderWithProviders(<Navbar />);
+
+    expect(
+      screen.getByRole("button", { name: /dashboards/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /Test User/i })
     ).toBeInTheDocument();
   });
 });
